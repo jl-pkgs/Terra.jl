3062e20cac1a2262bdf2dabe0a79670398f2a45d
Dongdong Kong <kongdd@users.noreply.github.com>
2024-03-22 10:13:18 +0800

    improve findnear

--- src/MFDataset/tools_Ipaper.jl
+++ src/MFDataset/tools_Ipaper.jl
@@ -14,15 +14,14 @@ function rm_empty(x::Vector)
   inds, x[inds]
 end
 
-# function findnear(values, x)
-#   _, i = findmin(abs.(values .- x))
-#   values[i], i
-# end
-
 dist(p1, p2) = sqrt((p1[1] - p2[1])^2 + (p1[2] - p2[2])^2)
 
 dist(p1, points::AbstractVector) = [dist(p1, p2) for p2 in points]
 
+# value
+findnear(x::Real, values::AbstractVector) = findmin(abs.(values .- x))[2]
+
+# point
 findnear(p1, points::AbstractVector) = findmin(dist(p1, points))[2]
 
 export findnear
